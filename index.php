63f75fb57cabc836d91dafb13849c3acfca8804b
Hafiz Caniago <hafizcode02@gmail.com>
2024-05-26 19:11:17 +0700

    fix: layout index.php

--- index.php
+++ index.php
@@ -36,50 +36,60 @@
 <body>
    <!--header section start -->
    <div class="container">
-      <nav class="navbar navbar-dark bg-dark">
-         <a class="logo" href="index.php"><h1>HOME</h1></a>
-         <div class="search_section">
-            <ul>
-               <li><a href="pages/login.php" style="font-family: Arial, Helvetica, sans-serif;"><button type="button" class="btn btn-light"><b>Login</b></button></a></li>
-            </ul>
-         </div>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <a class="navbar-brand" href="#">
+            <h1 style="padding-top: 10px;">Home</h1>
+         </a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
          </button>
-         <div class="collapse navbar-collapse" id="navbarsExample01">
-            <ul class="navbar-nav mr-auto">
-               <li class="nav-item active">
-                  <a class="nav-link" href="pages/category.php" style="font-family:Arial, Helvetica, sans-serif ;"><b>Pelatih</b></a>
-               </li>
+
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
                <li class="nav-item">
-                  <a class="nav-link" href="pages/login.php" style="font-family: Arial, Helvetica, sans-serif;"><b>Login</b></a>
+                  <a class="btn btn-light btn-login-text" href="pages/login.php" target="_blank">Login</a>
                </li>
             </ul>
-
          </div>
       </nav>
    </div>
    <!--banner section start -->
    <div class="container">
-      <div class="images/ahs.jpeg"><img src="images/ahs.jpeg" alt="images/ahs.jpeg" height="8000" width="8000"></div>
-      <div id="my_slider" class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
          <div class="carousel-inner">
             <div class="carousel-item active">
-               <div class="row">
-                  <div class="col-md-6">
-                     <div class="taital_main">
-                        <p class="banner_text" style="color : #000000">Dalam hadis riwayat Imam Bukhari RA, Nabi Muhammad SAW menganjurkan para sahabatnya dan seluruh umat Islam yang mengikuti sunnahnya untuk menguasai berbagai bidang olahraga, terutama berkuda, berenang, dan memanah.Inilah saatnya kita berolahraga sunnah sesuai anjuran rasulullah SAW </p>
-                        <div class="book_bt"><a href="pages/login.php" style="font-family: Arial, Helvetica, sans-serif;">Pembelian Tiket</a></div>
-                     </div>
-                  </div>
-               </div>
+               <img class="d-block w-100" src="images/ahs.jpeg" alt="First slide">
             </div>
+            <div class="carousel-item">
+               <img class="d-block w-100" src="images/ahs.jpeg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+               <img class="d-block w-100" src="images/ahs.jpeg" alt="Third slide">
+            </div>
+         </div>
+         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+         </a>
+      </div>
+   </div>
+
+   <div class="container mt-5">
+      <div class="row">
+         <div class="taital_main">
+            <p class="banner_text" style="color : #000000; text-align: justify; font-size: 18pt; font-family: 'Times New Roman', Times, serif;">
+               "Dalam hadis riwayat Imam Bukhari RA, Nabi Muhammad SAW menganjurkan para sahabatnya dan seluruh umat Islam yang mengikuti sunnahnya untuk menguasai berbagai bidang olahraga, terutama berkuda, berenang, dan memanah.
+               Inilah saatnya kita berolahraga sunnah sesuai anjuran rasulullah SAW"
+            </p>
+            <div class="book_bt"><a href="pages/login.php" style="font-family: Arial, Helvetica, sans-serif;">Beli Tiket</a></div>
          </div>
       </div>
    </div>
-   </div>
    <!--banner section end -->
-   </div>
    <!--header section end -->
    <!--category section start -->
    <div class="container">
@@ -91,7 +101,7 @@
             <div class="col-lg-10 col-sm-12 main">
                <div class="col">
                   <div class="box_main">
-                     <div class="images/memanah.jpeg"><img src="images/memanah.jpeg" alt="images/memanah.jpeg" height="70" width="70"></div>
+                     <div class="images/memanah.jpeg"><img src="images/memanah.jpeg" alt="images/memanah.jpeg" height="80" width="80"></div>
                      <h4 class="fashion_text">Archery Session</h4>
                   </div>
                </div>
@@ -109,14 +119,12 @@
                </div>
             </div>
          </div>
-
       </div>
-      <!-- category section end -->
+   </div>
+   <!-- category section end -->
 
-      <!-- product section start -->
-
-      <!-- product section end -->
-      <!-- client section start -->
+   <!-- client section start -->
+   <div class="container">
       <div class="client_section layout_padding">
          <div id="main_slider" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
